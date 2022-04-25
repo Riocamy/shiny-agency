98f17ee7ae9eb24e706899b15317e07ff7557fc6
Chlodion Gossart <chlodiongossart@gmail.com>
2022-04-25 17:13:53 +0200

    added prettier file

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './style/index.css';
-import Home from './pages/Home';
+import Home from './pages/Home/Home';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
