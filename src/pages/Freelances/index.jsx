e023cb05e0b050d650e45f0a055d95a4db943df7
Chlodion Gossart <chlodiongossart@gmail.com>
2022-04-26 19:37:25 +0200

    added cards with  proptypes

--- src/pages/Freelances/index.jsx
+++ src/pages/Freelances/index.jsx
@@ -1,9 +1,34 @@
+import Card from '../../components/Card'
+ 
+const freelanceProfiles = [
+    {
+      name: 'Jane Doe',
+      jobTitle: 'Devops',
+    },
+    {
+      name: 'John Doe',
+      jobTitle: 'Developpeur frontend',
+    },
+    {
+      name: 'Jeanne Biche',
+      jobTitle: 'Développeuse Fullstack',
+    },
+]
+ 
 function Freelances() {
   return (
-    <div>
-      <h1>Freelances 👩‍💻👨‍💻👩‍💻</h1>
-    </div>
+      <div>
+          <h1>Freelances 👩‍💻👨‍💻👩‍💻</h1>
+          {freelanceProfiles.map((profile, index) => (
+              <Card
+                key={`${profile.name}-${index}`}
+                label={profile.jobTitle}
+                picture={profile.picture}
+                title={profile.name}
+              />
+          ))}
+      </div>
   )
 }
-
+ 
 export default Freelances
