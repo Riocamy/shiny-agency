dc04c983531c1178177c34407e919e87c5178f34
Chlodion Gossart <chlodiongossart@gmail.com>
2022-05-02 15:05:16 +0200

    added results tests with Jest

---
+++ frontend/src/pages/Results/index.test.js
@@ -0,0 +1,25 @@
+import { formatJobList, formatQueryParams } from './'
+
+describe('The formatJobList function', () => {
+  it('should add a comma to a word', () => {
+    const expectedState = 'item2,'
+    expect(formatJobList('item2', 3, 1)).toEqual(expectedState)
+  })
+  it('should not add a comma to the last element of the list', () => {
+    const expectedState = 'item3'
+    expect(formatJobList('item3', 3, 2)).toEqual(expectedState)
+  })
+})
+
+describe('The formatQueryParams function', () => {
+  it('should use the right format for param', () => {
+    const expectedState = 'a1=answer1'
+    expect(formatQueryParams({ 1: 'answer1' })).toEqual(expectedState)
+  })
+  it('should concatenate params with an &', () => {
+    const expectedState = 'a1=answer1&a2=answer2'
+    expect(formatQueryParams({ 1: 'answer1', 2: 'answer2' })).toEqual(
+      expectedState
+    )
+  })
+})
