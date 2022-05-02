0f57edae35170b38f254ca63b88008ac6035a531
Chlodion Gossart <chlodiongossart@gmail.com>
2022-05-02 15:53:45 +0200

    added favorite features on cards and tests

---
+++ frontend/src/components/Card/index.test.js
@@ -0,0 +1,39 @@
+import Card from './'
+import { render, screen, fireEvent } from '@testing-library/react'
+import { ThemeProvider } from '../../utils/context'
+
+
+describe('Card', () => {
+  //Vérifier si une image et un titre sont affichés sur la carte
+  test('Should render title and image', async () => {
+    render(
+      <ThemeProvider>
+        <Card
+          title="Harry Potter"
+          label="Magicien frontend"
+          picture="/myPicture.png"
+        />
+      </ThemeProvider>
+    )
+    const cardPicture = screen.getByRole('img')
+    const cardTitle = screen.getByText(/Harry/i)
+    expect(cardPicture.src).toBe('http://localhost/myPicture.png')
+    expect(cardTitle.textContent).toBe(' Harry Potter ')
+  })
+  //Vérifier si une étoile est ajoutée quand la carte est ajoutée aux favoris
+  test('Should add ⭐️ around title', async () => {
+    render(
+      <ThemeProvider>
+        <Card
+          title="Harry Potter"
+          label="Magicien frontend"
+          picture="/myPicture.png"
+        />
+      </ThemeProvider>
+    )
+    const cardTitle = screen.getByText(/Harry/i)
+    const parentNode = cardTitle.closest('div')
+    fireEvent.click(parentNode)
+    expect(cardTitle.textContent).toBe('⭐️ Harry Potter ⭐️')
+  })
+})
