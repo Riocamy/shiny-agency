0b043371cbc01178329f2311ad7489dfb76ddbcd
Chlodion Gossart <chlodiongossart@gmail.com>
2022-05-03 22:58:53 +0200

    added dark mode test and mail input

--- frontend/src/components/Footer/index.jsx
+++ frontend/src/components/Footer/index.jsx
@@ -1,32 +1,35 @@
-import { useContext } from 'react'
-import { ThemeContext } from '../../utils/context'
+import { useTheme } from '../../utils/hooks'
 import styled from 'styled-components'
 import colors from '../../utils/style/colors'
- 
+import EmailInput from '../EmailInput'
+
 const FooterContainer = styled.footer`
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
-  padding-top: 60px;
+  padding: 60px 0;
 `
- 
+
 const NightModeButton = styled.button`
-    background-color: transparent;
-    border: none;
-    cursor: pointer;
-    color: ${colors.secondary};
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+  color: ${colors.secondary};
+  padding-top: 30px;
 `
- 
+
 function Footer() {
-  const { toggleTheme, theme } = useContext(ThemeContext)
+  const { toggleTheme, theme } = useTheme()
+
   return (
-      <FooterContainer>
+    <FooterContainer>
+      <EmailInput theme={theme} />
       <NightModeButton onClick={() => toggleTheme()}>
-          Changer de mode : {theme === 'light' ? '☀️' : '🌙'}
+        Changer de mode : {theme === 'light' ? '☀️' : '🌙'}
       </NightModeButton>
-      </FooterContainer>
+    </FooterContainer>
   )
 }
- 
+
 export default Footer
